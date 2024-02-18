da87b17e89806919622f756f218ab1cd1355200c
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2024-02-18 10:59:45 +0900

    Convert project from Java to Kotlin

---
+++ src/main/kotlin/com/github/sigureruri/yuquest/YuQuest.kt
@@ -0,0 +1,14 @@
+package com.github.sigureruri.yuquest
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class YuQuest : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+        println("TEST")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
